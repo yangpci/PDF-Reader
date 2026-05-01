1c3cfb95cf0654ae7668e39cd2d076bb072e8c7f
yangpci <yangpci@163.com>
2026-05-01 23:26:47 +0800

    Initial Commit

---
+++ PDF-Reader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PDF-Reader
+//
+//  Created by yangpci on 2026/5/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PDF-Reader/PDF_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_ReaderApp.swift
+//  PDF-Reader
+//
+//  Created by yangpci on 2026/5/1.
+//
+
+import SwiftUI
+
+@main
+struct PDF_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
